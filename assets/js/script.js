2fa0d711d3483903b32e01d01c80d7ed64917cdd
Sturgart <wesleyicaro4@gmail.com>
2022-05-10 19:08:48 -0300

    Correção

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var currentNumber = 0
 function increment() {
   currentNumber = currentNumber + 1
   currentNumberWrapper.innerHTML = currentNumber
-  if (currentNumber => 0) {
+  if (currentNumber > 0) {
     currentNumberWrapper.style.color = 'grey'
   }
 }
